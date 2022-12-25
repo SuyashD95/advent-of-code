3b874cd518bb07d12edc4a0f64ed44ac1cf205bc
Suyash <suyash.d@netwebindia.com>
2022-12-25 13:08:53 +0530

    Add the boilerplate code for puzzle 1 of Day 2 of AoC 2022

---
+++ day-2/day-2-solution.py
@@ -0,0 +1,21 @@
+"""
+Advent of Code 2022
+-------------------
+Day 2: Rock Paper Scissors
+
+Part 1 (Pending):
+    Question: What would your total score be if everything goes exactly
+    according to your strategy guide?
+    Answer:
+"""
+
+
+def part_1_solution(puzzle_input_filename: str) -> int:
+    """Return the answer to the question asked in 1st puzzle based
+    on the given input.
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"Day 2 (Part 1) Answer: {part_1_solution('day-2-input.txt')}")

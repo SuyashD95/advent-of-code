01ee8634d5db60f2d16a9d2d68fe37b7d149f6b6
Suyash <suyash.d@netwebindia.com>
2023-01-13 16:31:19 +0530

    Add constants & scoring scheme for 2nd day puzzles

--- day-2/day-2-solution.py
+++ day-2/day-2-solution.py
@@ -8,6 +8,34 @@ Part 1 (Pending):
     according to your strategy guide?
     Answer:
 """
+from _typeshed import ReadOnlyBuffer
+
+
+LOSS_PTS = 0
+DRAW_PTS = 3
+WIN_PTS = 6
+
+ROCK_PTS = 1
+PAPER_PTS = 2
+SCISSORS_PTS = 3
+
+SCORING_SYSTEM = {
+    "Rock": {
+        "Rock": ROCK_PTS + DRAW_PTS,
+        "Paper": ROCK_PTS + LOSS_PTS,
+        "Scissors": ROCK_PTS + WIN_PTS
+    },
+    "Paper": {
+        "Rock": PAPER_PTS + WIN_PTS,
+        "Paper": PAPER_PTS + DRAW_PTS,
+        "Scissors": PAPER_PTS + LOSS_PTS
+    },
+    "Scissors": {
+        "Rock": SCISSORS_PTS + LOSS_PTS,
+        "Paper": SCISSORS_PTS + WIN_PTS,
+        "Scissors": SCISSORS_PTS + DRAW_PTS
+    }
+}
 
 
 def part_1_solution(puzzle_input_filename: str) -> int:

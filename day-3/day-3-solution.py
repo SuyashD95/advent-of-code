a7457774d616f50333aeb6c7802f0a86b8cb805d
Suyash <suyash.d@netwebindia.com>
2023-01-14 23:07:10 +0530

    Add boilerplate code for Day 3 solution code

---
+++ day-3/day-3-solution.py
@@ -0,0 +1,23 @@
+"""
+Advent of Code 2022
+-------------------
+Day 3: Rucksack Reorganization
+
+Part 1 (Pending):
+    Question: Find the item type that appears in both compartments of each
+    rucksack. What is the sum of priorities of those item types?
+    Answer:
+"""
+
+
+def common_items_total_priority(puzzle_input_filename: str) -> int:
+    """Returns the sum of the priorities of items that have been found to
+    be placed on both the compartments (having the same size) of a rucksack.
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    puzzle_input_filename = "day-3-input.txt"
+    print("Day 3 Answers:")
+    print(f"\tPart 1: {common_items_total_priority(puzzle_input_filename)}")

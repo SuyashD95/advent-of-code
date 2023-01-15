6b22f336d98f4e034852f0883fc4f5af96f6e670
Suyash <suyash.d@netwebindia.com>
2023-01-15 17:40:35 +0530

    Implement common_items_total_priority() that splits rucksack items into 2 halves & compares them using their position in compartment

--- day-3/day-3-solution.py
+++ day-3/day-3-solution.py
@@ -19,8 +19,22 @@ ITEM_PRIORITY_MAP = {
 def common_items_total_priority(puzzle_input_filename: str) -> int:
     """Returns the sum of the priorities of items that have been found to
     be placed on both the compartments (having the same size) of a rucksack.
+
+    Parameters
+    ----------
+    puzzle_input_filename: Name of the text file containing puzzle dataset.
     """
-    return 0
+    total_priority = 0
+    with open(puzzle_input_filename) as puzzle_file:
+        while rucksack_items := puzzle_file.readline()[:-1]:
+            compartment_size = len(rucksack_items) // 2
+            left_compartment = rucksack_items[:compartment_size]
+            right_compartment = rucksack_items[compartment_size:]
+            for position in range(compartment_size):
+                if left_compartment[position] == right_compartment[position]:
+                    total_priority += ITEM_PRIORITY_MAP[left_compartment[position]]
+                    break
+    return total_priority
 
 
 if __name__ == "__main__":

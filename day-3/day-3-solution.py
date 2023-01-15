f09b744bdf42268c3a8b01da161f5bb225da4b0f
Suyash <suyash.d@netwebindia.com>
2023-01-15 18:15:52 +0530

    Add boilerplate code for function that will implement solution for 2nd puzzle for Day 3 of AoC 2022

--- day-3/day-3-solution.py
+++ day-3/day-3-solution.py
@@ -1,12 +1,17 @@
 """
 Advent of Code 2022
 -------------------
-Day 3: Rucksack Reorganization
+Day 3: Rucksack Reorganization (Part 1 Finished)
 
-Part 1 (Pending):
+Part 1 (Completed):
     Question: Find the item type that appears in both compartments of each
     rucksack. What is the sum of priorities of those item types?
-    Answer:
+    Answer: 8053
+
+Part 2 (Pending):
+    Question: Find the item type that corresponds to the badges of each
+    three-Elf group. What is the sum of the priorities of those item types?
+    Answer: 
 """
 from string import ascii_letters
 
@@ -15,7 +20,7 @@ ITEM_PRIORITY_MAP = {
     for index, letter in enumerate(ascii_letters)
 }
 
-
+# -------------------------- PART 1 SOLUTION -----------------------------
 def common_items_total_priority(puzzle_input_filename: str) -> int:
     """Returns the sum of the priorities of items that have been found to
     be placed on both the compartments (having the same size) of a rucksack.
@@ -28,16 +33,35 @@ def common_items_total_priority(puzzle_input_filename: str) -> int:
     with open(puzzle_input_filename) as puzzle_file:
         while rucksack_items := puzzle_file.readline()[:-1]:
             compartment_size = len(rucksack_items) // 2
-            left_compartment = rucksack_items[:compartment_size]
-            right_compartment = rucksack_items[compartment_size:]
-            for position in range(compartment_size):
-                if left_compartment[position] == right_compartment[position]:
-                    total_priority += ITEM_PRIORITY_MAP[left_compartment[position]]
-                    break
+            left_compartment_itemset = set(rucksack_items[:compartment_size])
+            right_compartment_itemset = set(rucksack_items[compartment_size:])
+            common_item = (left_compartment_itemset & right_compartment_itemset).pop()
+            total_priority += ITEM_PRIORITY_MAP[common_item]
     return total_priority
+# ------------------------------------------------------------------------
+
+
+# -------------------------- PART 2 SOLUTION -----------------------------
+def elfgroup_badges_total_priority(puzzle_input_filename: str) -> int:
+    """Returns the sum of the priorities of badge items that have been found
+    in the rucksacks of each member of a three-elf group.
+
+    Parameters
+    ----------
+    puzzle_input_filename: Name of the text file containing puzzle dataset.
+    """
+    total_badge_priority = 0
+    with open(puzzle_input_filename) as puzzle_file:
+        while rucksack_items := puzzle_file.readline()[:-1]:
+            pass
+    return total_badge_priority
+# ------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
     puzzle_input_filename = "day-3-input.txt"
-    print("Day 3 Answers:")
-    print(f"\tPart 1: {common_items_total_priority(puzzle_input_filename)}")
+    print(
+        "Day 3 Answers:\n"
+        f"  Part 1: {common_items_total_priority(puzzle_input_filename)}\n"
+        f"  Part 2: {elfgroup_badges_total_priority(puzzle_input_filename)}"
+    )

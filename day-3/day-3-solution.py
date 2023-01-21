c33be88c18d7954791c81a10f92d3060aa995850
Suyash <suyash.d@netwebindia.com>
2023-01-21 13:01:46 +0530

    Complete implementation for elfgroup_badges_total_priority() to solve 2nd puzzle for 3rd day of AoC 2022 challenge

--- day-3/day-3-solution.py
+++ day-3/day-3-solution.py
@@ -1,24 +1,27 @@
 """
 Advent of Code 2022
 -------------------
-Day 3: Rucksack Reorganization (Part 1 Finished)
+Day 3: Rucksack Reorganization (Finished)
 
 Part 1 (Completed):
     Question: Find the item type that appears in both compartments of each
     rucksack. What is the sum of priorities of those item types?
     Answer: 8053
 
-Part 2 (Pending):
+Part 2 (Completed):
     Question: Find the item type that corresponds to the badges of each
     three-Elf group. What is the sum of the priorities of those item types?
-    Answer: 
+    Answer: 2425
 """
+from typing import cast
+from functools import reduce
 from string import ascii_letters
 
 ITEM_PRIORITY_MAP = {
     letter: index + 1
     for index, letter in enumerate(ascii_letters)
 }
+
 
 # -------------------------- PART 1 SOLUTION -----------------------------
 def common_items_total_priority(puzzle_input_filename: str) -> int:
@@ -35,7 +38,9 @@ def common_items_total_priority(puzzle_input_filename: str) -> int:
             compartment_size = len(rucksack_items) // 2
             left_compartment_itemset = set(rucksack_items[:compartment_size])
             right_compartment_itemset = set(rucksack_items[compartment_size:])
-            common_item = (left_compartment_itemset & right_compartment_itemset).pop()
+            common_item = (
+                left_compartment_itemset & right_compartment_itemset
+            ).pop()
             total_priority += ITEM_PRIORITY_MAP[common_item]
     return total_priority
 # ------------------------------------------------------------------------
@@ -51,9 +56,26 @@ def elfgroup_badges_total_priority(puzzle_input_filename: str) -> int:
     puzzle_input_filename: Name of the text file containing puzzle dataset.
     """
     total_badge_priority = 0
+    ELF_GROUP_SIZE = 3
+
     with open(puzzle_input_filename) as puzzle_file:
+        elf_group_rucksacks: list[str] = []
+
         while rucksack_items := puzzle_file.readline()[:-1]:
-            pass
+            elf_group_rucksacks.append(rucksack_items)
+            if not len(elf_group_rucksacks) == ELF_GROUP_SIZE:
+                continue
+
+            group_badge = cast(str, reduce(
+                lambda elf_bag_1, elf_bag_2: elf_bag_1 & elf_bag_2,
+                map(
+                    lambda rucksack: set(rucksack),
+                    elf_group_rucksacks
+                )
+            ).pop())
+
+            total_badge_priority += ITEM_PRIORITY_MAP[group_badge]
+            elf_group_rucksacks.clear()
     return total_badge_priority
 # ------------------------------------------------------------------------
 

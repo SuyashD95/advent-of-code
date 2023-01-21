84992e4cf4d5807f012c3790c93b3cb8aada9aaf
Suyash <suyash.d@netwebindia.com>
2023-01-21 17:35:45 +0530

    Merge the 2 utility functions into a single function

--- day-4/day-4-solution.py
+++ day-4/day-4-solution.py
@@ -13,12 +13,13 @@ Part 2 (Pending):
 """
 
 
-def is_1st_pair_subset_of_2nd_pair(
+# ========================== PART ONE SOLUTION ===========================
+def is_one_range_subset_of_another_range(
         first_pair: tuple[int, int],
         second_pair: tuple[int, int]
         ) -> bool:
-    """Returns True if the range of numbers represented by the first pair is a subset
-    of the range numbers represented by the second pair.
+    """Returns True if the range of numbers represented by the one pair is a subset
+    of the range numbers represented by the another pair.
     """
     first_pair_low_bound = first_pair[0]
     first_pair_high_bound = first_pair[1]
@@ -30,22 +31,7 @@ def is_1st_pair_subset_of_2nd_pair(
         and first_pair_high_bound <= second_pair_high_bound
     ):
         return True
-    return False
-
-
-def is_2nd_pair_subset_of_1st_pair(
-        first_pair: tuple[int, int],
-        second_pair: tuple[int, int]
-        ) -> bool:
-    """Returns True if the range of numbers represented by the second pair is
-    a subset of the range numbers represented by the first pair pair.
-    """
-    first_pair_low_bound = first_pair[0]
-    first_pair_high_bound = first_pair[1]
-    second_pair_low_bound = second_pair[0]
-    second_pair_high_bound = second_pair[1]
-
-    if (
+    elif (
         second_pair_low_bound >= first_pair_low_bound
         and second_pair_high_bound <= first_pair_high_bound
     ):
@@ -69,12 +55,10 @@ def subset_assignment_pairs_count(puzzle_input_filename: str) -> int:
             first_pair = (int(first_pair[0]), int(first_pair[1]),)
             second_pair = second_section.split("-")
             second_pair = (int(second_pair[0]), int(second_pair[1]),)
-            if (
-                is_1st_pair_subset_of_2nd_pair(first_pair, second_pair)
-                or is_2nd_pair_subset_of_1st_pair(first_pair, second_pair)
-            ):
+            if is_one_range_subset_of_another_range(first_pair, second_pair):
                 count += 1
     return count
+# ========================================================================
 
 
 if __name__ == "__main__":

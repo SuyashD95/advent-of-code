9fbfcd1a5b215b1d491c69c0b86c1261adb46a8d
Suyash <suyash.d@netwebindia.com>
2023-01-21 13:34:22 +0530

    Add placeholder code for Day 4 solution

---
+++ day-4/day-4-solution.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code 2022
+-------------------
+Day 4: Camp Cleanup (Pending)
+
+Part 1 (Pending):
+    Question: In how many assignment pairs does one range fully contain the other?
+    Answer:
+"""
+
+def puzzle_1_solution(puzzle_input_filename: str) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    puzzle_input_filename = "day-4-input.txt"
+    print(
+        "Day 4 Answers:\n"
+        f"  Part 1: {puzzle_1_solution(puzzle_input_filename)}\n"
+    )

f2993966b4f29f99418fe6846b83c73ae6177248
Suyash <suyash.d@netwebindia.com>
2023-01-31 23:35:37 +0530

    Add boilerplate code for solving 2nd puzzle of Day 5

--- day-5/day-5-solution.py
+++ day-5/day-5-solution.py
@@ -3,10 +3,15 @@ Advent of Code 2022
 -------------------
 Day 5: Supply Stacks (Part 1 Finished)
 
-Part 1 (Finished):
+Part 1 (Completed):
     Question: After the rearrangement procedure completes, what crate
     ends up on top of each stack?
     Answer: FCVRLMVQP
+
+Part 2 (Pending):
+    Question: After rearrangement procedure (performed by CrateMover9001)
+    completes, what crate ends up on top each stack?
+    Answer:
 """
 from __future__ import annotations
 from typing import TYPE_CHECKING, TypeAlias, TypedDict
@@ -159,9 +164,46 @@ def list_crates_at_top_of_rearranged_stacks(puzzle_input_filename: str) -> str:
 # ------------------------------------------------------------------------
 
 
+# -------------------------- PART TWO SOLUTION ---------------------------
+def list_crates_at_top_of_rearranged_stacks_using_9001(
+    puzzle_input_filename: str
+) -> str:
+    """Returns a string containing the name of crates that are placed at
+    the top of the stacks after they are rearrangement procedure mentioned
+    in the input file is performed.
+
+    NOTE: In this rearrangement procedure, CrateMover9001 crane is used which
+    is capable of picking up and moving multiple crates at once. This means
+    that the crates being moved remain in the same order.
+
+    Parameters
+    ----------
+    puzzle_input_filename: Name of the file containing input dataset.
+
+    Returns
+    -------
+    A string containing name of crates that are placed at top of stacks.
+    """
+    with open(puzzle_input_filename) as puzzle_file:
+        crate_matrix = make_crates_matrix(puzzle_file)
+        movelist = create_movelist(puzzle_file)
+        crate_stacks = extract_crate_stacks(crate_matrix)
+
+        for move_command in movelist:
+            move_crates(crate_stacks, move_command)
+
+        top_crate_list = []
+        for stack in crate_stacks.values():
+            top_crate_list.append(stack[-1])
+
+    return "".join(top_crate_list)
+# ------------------------------------------------------------------------
+
+
 if __name__ == "__main__":
     puzzle_input_filename = "day-5-input.txt"
     print(
         "Day 5 Answers:\n"
         f"  Part 1: {list_crates_at_top_of_rearranged_stacks(puzzle_input_filename)}\n"
+        f"  Part 2: {list_crates_at_top_of_rearranged_stacks_using_9001(puzzle_input_filename)}"
     )

55e477126876211396019008403db109345f2ffb
Suyash <suyash.d@netwebindia.com>
2023-02-28 23:56:51 +0530

    Add boilerplate code for Day 7 module

---
+++ day-7/day-7-solution.py
@@ -0,0 +1,13 @@
+"""
+Advent of Code 2022
+-------------------
+Day 7: No Space Left On Device (Pending)
+
+Part 1 (Pending):
+    Question: How many characters need to be processed before the first
+    start-of-packet marker is detected?
+    Answer:
+"""
+
+if __name__ == "__main__":
+    puzzle_input_filename = "day-7-input.txt"

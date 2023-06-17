b420df4a8a9b23944cbff11b6d678f8cffb69c5e
Suyash Dayal <suyash.d@netwebindia.com>
2023-06-17 17:29:12 +0530

    Add initial code for the solution of Part 1 puzzle for day 7

--- day-7/day-7-solution.py
+++ day-7/day-7-solution.py
@@ -7,8 +7,65 @@ Part 1 (Pending):
     Question: What is the sum of the total sizes of those directories?
     Answer:
 """
+import typing
+
+from dataclasses import dataclass
+from enum import Enum, auto
+
+# Initialization
+# --------------
+fs_root: FSElement | None
+cwd: FSElement | None
 
 
+# Filesystem
+# ----------
+class FSType(Enum):
+    """Type of node in filesystem."""
+
+    DIRECTORY = auto()
+    FILE = auto()
+
+
+@dataclass
+class FSElement:
+    """A representation of a file/directory in the filesystem."""
+
+    fs_type: FSType
+    children: list[FSElement]
+    size: int
+    name: str
+    parent: FSElement | None
+
+
+# Parser
+# ------
+class InputParser:
+
+    def __init__(self, termstream: typing.TextIO) -> InputParser
+        """Initializes an object.
+        
+        Parameters
+        ----------
+        termstream: A file containing a set of commands and their outputs
+        as present in a virtual terminal application.
+        """
+        self.termstream = termstream
+
+    def parse_command(self, command: str) -> list[str]:
+        """Parse the given command in the file to perform some action in
+        the virtual filesystem and move the file pointer to the next
+        statement to be executed.
+
+        Parameters
+        ----------
+        command: A string representing a command of the virtual OS.
+        """
+        pass
+            
+
+# Part 1 Solution
+# ---------------
 def sum_of_directories_under_100k(puzzle_input_filename: str) -> int:
     """Returns the total size of all directories whose size is less than or
     equal to 100K present in the filesystem represented by the provided puzzle

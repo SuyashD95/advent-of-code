c5bb290cb36bfe68b6c1062c92185bc1eb267feb
Suyash Dayal <suyash.d@netwebindia.com>
2023-06-23 14:13:48 +0530

    Add rough implementation for parse_command method, focusing on processing 'ls' command

--- day-7/day-7-solution.py
+++ day-7/day-7-solution.py
@@ -54,23 +54,51 @@ class InputParser:
         """This function is used for parsing the given stream and
         constructing the virtual filesystem on which further operations
         can be performed.
+
+        Returns
+        -------
+        The root element of the FSTree, representing the '/' directory
+        of the filesystem.
         """
+        COMMAND_IDENTIFIER = "$"
+
         with open(puzzle_input_filename) as puzzle_file:
             while terminal_output := puzzle_file.readline()[:-1]:
                 print(terminal_output)
-
+                output_terms = terminal_output.split()
+                if output_terms[0] == COMMAND_IDENTIFIER:
+                    self.parse_command(output_terms[1:], puzzle_file)
         return self.fs_root
 
-    def parse_command(self, command: str) -> list[str]:
+    def parse_command(self, cmd_terms: list[str], file_handler: typing.TextIO) -> list[str]:
         """Parse the given command in the file to perform some action in
         the virtual filesystem and move the file pointer to the next
         statement to be executed.
 
         Parameters
         ----------
-        command: A string representing a command of the virtual OS.
+        cmd_terms: A list of strings representing the terms of a command on
+        the virtual OS.
+        file_handler: Pointer operating on the given file.
         """
-        pass
+        print(f"Command Terms: {cmd_terms}")
+        command = cmd_terms[0]
+        print(f"Command Type: {command}")
+        if command == "ls":
+            while True:
+                current_position_in_file = file_handler.tell()
+                print(f"Current position in file: {current_position_in_file}")
+                next_line = file_handler.readline()[:-1]
+                if next_line:
+                    print(next_line)
+                    print(f"New position in file after reading: {file_handler.tell()}")
+                    if next_line[0] == "$":
+                        file_handler.seek(current_position_in_file)
+                        print(f"Position in file after resetting: {file_handler.tell()}")
+                        break
+                else:
+                    print(f"No newline to parse. Position at EOF: {file_handler.tell()}")
+                    break
             
 
 # Part 1 Solution

a2ae4bb160f45d80ab47916b50fa5a2bca58c10d
Suyash Dayal <suyash.d@netwebindia.com>
2023-09-21 11:34:30 +0530

    Define TreeMap class to store information about tree heights & corresponding metadata

--- day-8/day-8-solution.py
+++ day-8/day-8-solution.py
@@ -10,6 +10,18 @@ Part 1 (Pending):
 
 Author: Suyash Dayal
 """
+from dataclasses import dataclass
+
+
+@dataclass
+class TreeMap:
+    """This dataclass stores information about the different heights
+    of the trees in a given regiona and its associated metadata.
+    """
+    trees: list[list[int]]
+    height: int = 0
+    width: int = 0
+
 
 # Part 1 Solution
 # ---------------
@@ -24,7 +36,18 @@ def find_visible_trees_count(puzzle_input_filename: str) -> int:
     -------
     Number of trees visible from outside the grid.
     """
-    pass
+    tree_map_matrix = []
+    with open(puzzle_input_filename) as puzzle_file:
+        while output_line := puzzle_file.readline()[:-1]:
+            tree_row = [int(digit) for digit in output_line]
+            tree_map_matrix.append(tree_row)
+    tree_map = TreeMap(
+        trees=tree_map_matrix,
+        height=len(tree_map_matrix),
+        width=len(tree_map_matrix[0]) if len(tree_map_matrix) else 0 
+    )
+    print(tree_map)
+
 
 
 if __name__ == "__main__":

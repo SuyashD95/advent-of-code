1070be6531dcf859fce73f703b755f513e9d56cb
Suyash Dayal <suyash.d@netwebindia.com>
2023-09-25 11:02:51 +0530

    Add files for day 9 puzzles & update README.md

---
+++ day-9/day-9-solution.py
@@ -0,0 +1,13 @@
+"""
+Advent of Code 2022
+-------------------
+Day 9: Rope Bridge (Pending)
+
+Part 1 (Pending):
+    Question: Simulate your complete hypothetical series of motions. How many positions
+    does the tail of the rope visit at least once?
+    Answer:
+-------------------
+
+Author: Suyash Dayal
+"""

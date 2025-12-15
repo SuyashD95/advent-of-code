b37a5ab1afedc26817b4d95087d1926a97da07c0
Suyash Dayal <suyash.d@netwebindia.com>
2025-12-15 17:10:18 +0530

    feat: Add Rotation type and MakeRotation parser

---
+++ 2025/day-1/internal/rotations.go
@@ -0,0 +1,41 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Rotation struct {
+	direction string
+	steps     int
+}
+
+/* Returns a new `Rotation` based on the string given in the "<direction><steps>" format. */
+func MakeRotation(rotation string) (*Rotation, error) {
+	code := strings.TrimSpace(rotation)
+	if code == "" {
+		return nil, errors.New("No data in given rotation code")
+	}
+	var direction string
+	switch {
+	case strings.HasPrefix(code, "L") == true:
+		direction = "L"
+	case strings.HasPrefix(code, "R") == true:
+		direction = "R"
+	default:
+		return nil, fmt.Errorf("Invalid direction: '%v'", string(code[0]))
+	}
+	Sstep, _ := strings.CutPrefix(code, direction)
+	steps, err := strconv.Atoi(Sstep)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid step value: %w", err)
+	} else if steps < 0 {
+		return nil, fmt.Errorf("`Rotation.steps` ('%d') must be integer greater than 0", steps)
+	}
+	return &Rotation{
+		direction: direction,
+		steps:     steps,
+	}, nil
+}

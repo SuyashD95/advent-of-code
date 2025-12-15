617e8733e4f1069e9b4c62547e2d84d64f61f21c
Suyash Dayal <suyash.d@netwebindia.com>
2025-12-15 17:10:52 +0530

    impr: Extract getRotationsFromInput to parse rotations

--- 2025/day-1/main.go
+++ 2025/day-1/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"suyashd95/aoc/2025/internal"
 )
 
-func main() {
-	const absPath = "input.txt"
+/* Returns a slice containing rotations that has to be processed to get the answer. */
+func getRotationsFromInput(absPath string) []internal.Rotation {
+	rotations := make([]internal.Rotation, 0)
 	iFile, err := os.Open(absPath)
 	if err != nil {
 		log.Fatalf("Cannot read input.txt: %v\n", err)
@@ -17,7 +19,17 @@ func main() {
 
 	lnReader := bufio.NewScanner(iFile)
 	for lnReader.Scan() {
-		rotations := lnReader.Text()
-		fmt.Printf("%s\n", rotations)
+		rotation := lnReader.Text()
+		r, err := internal.MakeRotation(rotation)
+		if err != nil {
+			log.Fatalf("Illegal rotation encountered: %v", err)
+		}
+		rotations = append(rotations, *r)
 	}
+	return rotations
+}
+
+func main() {
+	rotationSeq := getRotationsFromInput("input.txt")
+	fmt.Println(rotationSeq)
 }

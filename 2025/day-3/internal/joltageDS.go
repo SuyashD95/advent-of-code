71cd67cc0290b9e6f7dc128925c3eb23d44d4f46
Suyash Dayal <suyash.d@netwebindia.com>
2025-12-19 15:38:11 +0530

    feat: Add CalculateOutput to BatteryBank
    Rename MAX_ONLINE_BATTERIES_IN_PACK to MAX_BATTERIES_IN_PACK. Import
    math for power calculation. Replace %w with %v in ToString error
    logging. Add concise comments for methods.

--- 2025/day-3/internal/joltageDS.go
+++ 2025/day-3/internal/joltageDS.go
@@ -3,10 +3,11 @@ package internal
 import (
 	"fmt"
 	"log"
+	"math"
 	"strings"
 )
 
-const MAX_ONLINE_BATTERIES_IN_PACK = 12
+const MAX_BATTERIES_IN_PACK = 12
 
 type Battery struct {
 	Joltage int
@@ -30,6 +31,7 @@ func NewBatteryList(joltage int) *BatteryBank {
 	}
 }
 
+/* Adds a new Battery element to the end of existing sequence of batteries. */
 func (bb *BatteryBank) Append(joltage int) {
 	var parentNode *Battery
 	for node := bb.Head; node != nil; node = node.Next {
@@ -41,18 +43,32 @@ func (bb *BatteryBank) Append(joltage int) {
 	}
 }
 
+/* Returns the total output that is produced by the sequence of
+ * batteries in the bank.
+ */
+func (bb BatteryBank) CalculateOutput() int64 {
+	var totalOutput int64
+	var currentDepth int
+	for node := bb.Head; node != nil; node = node.Next {
+		currentDepth += 1
+		totalOutput += int64(node.Joltage) * int64(math.Pow10(MAX_BATTERIES_IN_PACK-currentDepth))
+	}
+	return totalOutput
+}
+
+/* Prints a string representing the sequence of batteries present in the list */
 func (bb BatteryBank) ToString() string {
 	var builder strings.Builder
 	if _, err := builder.WriteString("H -> "); err != nil {
-		log.Fatalf("Unable to write head string into builder: %w", err)
+		log.Fatalf("Unable to write head string into builder: %v", err)
 	}
 	for node := bb.Head; node != nil; node = node.Next {
 		if _, err := builder.WriteString(fmt.Sprintf("%s -> ", node.ToString())); err != nil {
-			log.Fatalf("Unable to append battery string into builder: %w", err)
+			log.Fatalf("Unable to append battery string into builder: %v", err)
 		}
 	}
 	if _, err := builder.WriteString("X\n"); err != nil {
-		log.Fatalf("Unable to write EOL string into builder: %w", err)
+		log.Fatalf("Unable to write EOL string into builder: %v", err)
 	}
 	return builder.String()
 }
